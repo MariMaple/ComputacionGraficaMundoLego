2b0ee1253032191469f066cd3f58900f8ea55162
Mariana Arguelles <marianita_maple@comunidad.unam.mx>
2020-05-15 17:47:04 -0500

    actualizacion necesaria por error de debug en los spotlight

--- CommonValues.h
+++ CommonValues.h
@@ -2,6 +2,6 @@
 #define COMMONVALS
 #include "stb_image.h"
 
-const int MAX_POINT_LIGHTS = 10;
-const int MAX_SPOT_LIGHTS = 10;
+const int MAX_POINT_LIGHTS = 15;
+const int MAX_SPOT_LIGHTS = 15;
 #endif
